c332a0096dd4335b1f510b279cc19a9067d64835
John Gabriel Ylagan <gy.6.seneca@gmail.com>
2025-08-02 14:47:44 -0400

    Update logging.py

--- logging.py
+++ logging.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 # Student ID: jgylagan
-# Assignment 2
+# Assignment 2 NCC Group 4
 # Logging Function
 
-import datetime  
+from datetime import datetime  # for timestamps
 
-def write_log(message):
+def write_log(message):        # function to write a log entry
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M")  # year, month day , hour min format
+    filename = f"log_{timestamp}.txt" # save as log_y-d-h-m
+    
+    log_entry = f"[{timestamp}] {message}"  # log entry for the file output
 
+    # create new log  file in the current directory
+    with open(filename, "w") as f:
+        f.write(log_entry + "\n")  # \n newline formatting
+
+    print(f"Log written to: {filename}")  # show user where the log went, the same directory as the script
 
 
 if __name__ == "__main__":
-    write_log("Logged successfuly.")
+    write_log("Logged successfuly.") # terminal confimation
+
 
